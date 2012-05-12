58fdfb2ed9f3f71d372f5b994d82ebbfe2956f25
Tomo Krajina <tkrajina@gmail.com>
2012-05-12 06:40:40 +0200

    Još malo primjera s data tipovima

--- 011_data_types.hs
+++ 011_data_types.hs
@@ -15,7 +15,7 @@ data Maybe1 a = Nothing1
               | Just1 a
 
 {-
-To znači da tip Maybe a može biti ili Nothinig ili Just a
+To znači da tip Maybe a može biti ili Nothing ili Just a
 Npr. funkcija za prvi element liste bi mogla vraćati Maybe a
 -}
 
@@ -28,7 +28,26 @@ firstListElement :: [a] -> Maybe1 a
 firstListElement [] = Nothing1
 firstListElement (x:xs) = Just1 x
 
+-- Rekurzivni tip (lista):
+data List a = Nil
+            | Cons a (List a)
+
+listLength Nil = 0
+listLength (Cons x xs) = 1 + listLength xs
+
+-- enumerirani tipovi se mogu dobiti jednostavno s višestrukim praznim konstruktorima:
+data Color = Red
+           | Green
+           | Blue
+-- ako je potrebna specifična boja:
+--           | CustomColor Int Int Int
+
+getColor Red = (1, 0, 0)
+getColor Green = (0, 1, 0)
+getColor Blue = (0, 0, 1)
+--getColor (CustomColor r g b) = (r, g, b)
+
 main = do
     let pair = Pair 1 2
-    print (show (getFirstDataElement pair))
-    print (show (getSecondDataElement pair))
+    print (getFirstDataElement pair)
+    print (getSecondDataElement pair)
